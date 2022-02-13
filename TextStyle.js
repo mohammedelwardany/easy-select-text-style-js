1935372889ca53b789d545d6b81d559aa57bfb33
mohammedelwardany <78797469+mohammedelwardany@users.noreply.github.com>
2022-02-13 22:49:48 +0200

    Add files via upload

---
+++ TextStyle.js
@@ -0,0 +1,37 @@
+var text = document.getElementById("PAR")
+
+function ChangeFont(font) {
+    text.style.fontFamily = font;
+}
+
+function ChangeAlign(align) {
+    text.style.textAlign = align;
+}
+
+function ChangeHeight(height) {
+    text.style.lineHeight = height;
+}
+
+function ChangeLSpace(space) {
+    text.style.letterSpacing = space;
+}
+
+function ChangeIndent(indent) {
+    text.style.textIndent = indent;
+}
+
+function ChangeTransform(Transform) {
+    text.style.textTransform = Transform;
+}
+
+function ChangeDecorate(decoration) {
+    text.style.textDecoration = decoration;
+}
+
+function ChangeBorder(border) {
+    text.style.border = border;
+}
+
+function ChangeBorderColor(bordercolor) {
+    text.style.borderColor = bordercolor;
+}
